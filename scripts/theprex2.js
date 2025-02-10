0a72d14190ddcaa748a9f1c07bf1f3ebb9e3a657
Geonhwi Hwang <159416943+88gh@users.noreply.github.com>
2025-02-10 20:51:53 +0900

    Update theprex2.js

--- scripts/theprex2.js
+++ scripts/theprex2.js
@@ -1,12 +1,12 @@
-window.addEventListener('load', function() {
+window.addEventListener("load", function() {
   setTimeout(function() {
-    const header = document.getElementById('site-header');
-    header.classList.remove('splash-active');
-  }, 2000);
+    const header = document.getElementById("site-header");
+    // 1) remove splash-active
+    header.classList.remove("splash-active");
+    // 2) add scrolled
+    header.classList.add("scrolled");
+  }, 2000); // after 2 seconds
 });
-
-
-
 
 // REMOVE or COMMENT OUT anything like this:
 // window.addEventListener('scroll', function() {
